17f0c480e1b89add1c5ac81e6f38c78588d5265f
andrei9669 <ansiro@ttu.ee>
2021-08-25 10:37:41 +0300

    feat(cra template): update package versions in template

--- packages/cra-template-templeio/template/.eslintrc.js
+++ packages/cra-template-templeio/template/.eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    'shared-node-browser': true,
-    jest: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-    sourceType: 'module',
-  },
-  extends: ['@temple/eslint-config-temple/cra'],
+  extends: ['@templeio/eslint-config-templeio/cra'],
 };
