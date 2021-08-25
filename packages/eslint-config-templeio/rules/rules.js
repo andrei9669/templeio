3c5cb7155775094ad0e4f72dc0864b2dbba32f9c
andrei9669 <ansiro@ttu.ee>
2021-08-25 12:35:16 +0300

    fix(eslint): fix lint rules

--- packages/eslint-config-templeio/rules/rules.js
+++ packages/eslint-config-templeio/rules/rules.js
@@ -64,36 +64,4 @@ module.exports = {
     'sonarjs/no-identical-functions': 'warn',
     'sonarjs/no-collapsible-if': 'warn',
   },
-  overrides: [
-    {
-      files: ['**/!(setupTests|test/utils|*.spec.*|*.test.*).ts?x'],
-      rules: {
-        '@typescript-eslint/no-empty-function': 'error',
-        '@typescript-eslint/explicit-function-return-type': [
-          'error',
-          {
-            allowExpressions: true,
-          },
-        ],
-      },
-    },
-    {
-      parser: '@babel/eslint-parser',
-      parserOptions: {
-        requireConfigFile: false,
-      },
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/strict-boolean-expressions': 0,
-        '@typescript-eslint/naming-convention': 0,
-        '@typescript-eslint/dot-notation': 0,
-        '@typescript-eslint/no-implied-eval': 0,
-        '@typescript-eslint/no-throw-literal': 0,
-        '@typescript-eslint/no-var-requires': 0,
-        'import/no-extraneous-dependencies': 0,
-        '@typescript-eslint/return-await': 0,
-        'global-require': 0,
-      },
-    },
-  ],
 };
