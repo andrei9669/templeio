3d401ee913fe89dff780721bd2330defffab0031
andrei9669 <ansiro@ttu.ee>
2021-10-28 13:44:32 +0300

    feat(nextjs): add nextjs style rules

---
+++ packages/eslint-config-templeio/nextjs.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    browser: true,
+    'shared-node-browser': true,
+    jest: true,
+  },
+  extends: ['next/core-web-vitals', './react'],
+  overrides: [
+    {
+      files: ['src/pages/_document.tsx'],
+      rules: {
+        'react/display-name': 0,
+      },
+    },
+  ],
+};
