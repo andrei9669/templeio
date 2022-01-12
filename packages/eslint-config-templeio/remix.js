039ceca73a69622ff79d0bff011943eb1524671b
andrei9669 <ansiro@ttu.ee>
2022-01-12 11:40:17 +0200

    feat(new ruleset): add rules for remix app

---
+++ packages/eslint-config-templeio/remix.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    'shared-node-browser': true,
+    jest: true,
+  },
+  extends: ['./react'],
+  overrides: [],
+};
